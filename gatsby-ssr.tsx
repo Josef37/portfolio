e389c0c136b2df40ed3a94d9c2ce1cb94274fd1d
Josef Wittmann <josef.wittmann@tutanota.com>
2025-07-29 15:03:14 +0200

    Add types

--- gatsby-ssr.tsx
+++ gatsby-ssr.tsx
@@ -1,8 +1,9 @@
+import type { GatsbySSR } from 'gatsby';
 import React from 'react';
 
 const fontFiles = ['Montserrat-Latin.woff2', 'Montserrat-Latin-Italic.woff2'];
 
-export const onRenderBody = ({ setHeadComponents }) => {
+export const onRenderBody: GatsbySSR['onRenderBody'] = ({ setHeadComponents }) => {
   const fontHeadComponents = fontFiles.map(getFontHeadComponent);
 
   setHeadComponents([...fontHeadComponents]);
@@ -10,11 +11,11 @@ export const onRenderBody = ({ setHeadComponents }) => {
 
 const getFontHeadComponent = (filename: string) => (
   <link
+    key={filename}
     rel="preload"
     href={`/fonts/${filename}`}
     as="font"
     type="font/woff2"
     crossOrigin="anonymous"
-    key={filename}
   />
 );
